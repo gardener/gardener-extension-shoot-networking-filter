46400df12bf90d2fdafa2e42785900a1554044c2
Martin Weindel <martin.weindel@sap.com>
2025-05-05 16:53:15 +0200

    Support deployment on runtime cluster; support extension reconciliation for garden and seed

--- pkg/controller/lifecycle/add.go
+++ pkg/controller/lifecycle/add.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/gardener/gardener/extensions/pkg/controller/extension"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -36,11 +37,18 @@ type AddOptions struct {
 	ServiceConfig controllerconfig.Config
 	// IgnoreOperationAnnotation specifies whether to ignore the operation annotation or not.
 	IgnoreOperationAnnotation bool
+	// ExtensionClass defines the main extension class this extension is responsible for.
+	ExtensionClass extensionsv1alpha1.ExtensionClass
 }
 
 // AddToManager adds a Networking Policy Filter Lifecycle controller to the given Controller Manager.
 func AddToManager(ctx context.Context, mgr manager.Manager) error {
-	actuator, err := NewActuator(mgr, DefaultAddOptions.ServiceConfig.Configuration, DefaultAddOptions.ServiceConfig.OAuth2Secret)
+	extensionClasses := []extensionsv1alpha1.ExtensionClass{extensionsv1alpha1.ExtensionClassShoot, extensionsv1alpha1.ExtensionClassSeed}
+	if DefaultAddOptions.ExtensionClass == extensionsv1alpha1.ExtensionClassGarden {
+		extensionClasses = []extensionsv1alpha1.ExtensionClass{extensionsv1alpha1.ExtensionClassGarden}
+	}
+
+	actuator, err := NewActuator(mgr, DefaultAddOptions.ServiceConfig.Configuration, DefaultAddOptions.ServiceConfig.OAuth2Secret, extensionClasses)
 	if err != nil {
 		return err
 	}
@@ -53,5 +61,6 @@ func AddToManager(ctx context.Context, mgr manager.Manager) error {
 		Resync:            60 * time.Minute,
 		Predicates:        extension.DefaultPredicates(ctx, mgr, DefaultAddOptions.IgnoreOperationAnnotation),
 		Type:              constants.ExtensionType,
+		ExtensionClasses:  extensionClasses,
 	})
 }
